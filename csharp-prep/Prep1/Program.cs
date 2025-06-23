08e8e1d75116f86dd15a497b59edf0da2beed41c
Intchilz <chi21012@byui.edu>
2025-06-23 22:07:28 +0200

    Changed repo and moved all progress

---
+++ csharp-prep/Prep1/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Prep one is complete;
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+    
+}
